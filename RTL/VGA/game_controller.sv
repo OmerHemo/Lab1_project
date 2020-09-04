f5f0f7ed4399f153c28bc1194aae2cd9ba22deee
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-09-04 21:41:53 +0300

    breaking step fix!

--- RTL/VGA/game_controller.sv
+++ RTL/VGA/game_controller.sv
@@ -25,14 +25,13 @@ module	game_controller	(
 			output logic step_spike_collision,
 			output logic step_brake_collision,
 			output logic SingleHitPulse_regular_step // critical code, generating A single pulse in a frame
-			
 );
 
 
-assign step_regular_collision = (drawing_request_bumpy &&  (drawing_request_step_regular  == 1'b1)); 
+assign step_regular_collision = ((drawing_request_bumpy &&  (drawing_request_step_regular  == 1'b1)) || (step_brake_collision)); 
 assign prize_collision = (drawing_request_bumpy &&  (drawing_request_prize  == 1'b1)); 
 assign gate_collision = (drawing_request_bumpy &&  (drawing_request_gate  == 1'b1));
-assign step_free_collision = (drawing_request_bumpy &&  (drawing_request_step_free  == 1'b1)) || step_brake_collision ;
+assign step_free_collision = ((drawing_request_bumpy) &&  (drawing_request_step_free  == 1'b1));
 assign border_collision = (drawing_request_bumpy &&  (drawing_request_border  == 1'b1));
 assign step_spike_collision = (drawing_request_bumpy &&  (drawing_request_step_spike == 1'b1));
 assign step_brake_collision = (drawing_request_bumpy &&  (drawing_request_step_brake == 1'b1));
@@ -44,14 +43,14 @@ begin
 	if(!resetN)
 	begin 
 		flag	<= 1'b0;
-		SingleHitPulse_regular_step <= 1'b0 ; 
+		SingleHitPulse_regular_step <= 1'b0;
 	end 
 	else begin 
 
 			SingleHitPulse_regular_step <= 1'b0 ; // default 
 			if(startOfFrame) 
 				flag = 1'b0 ; // reset for next time 
-			if ((step_regular_collision)  && (flag == 1'b0)) begin 
+			if ((step_regular_collision)  && (flag == 1'b0)) begin
 				flag	<= 1'b1; // to enter only once 
 				SingleHitPulse_regular_step <= 1'b1 ; 
 			end ; 
