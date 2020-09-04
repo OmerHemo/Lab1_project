430031e7e27279f0540829bfe7517b7980fa45c7
OmerHemo <OmerHemo@users.noreply.github.com>
2020-09-04 16:43:03 +0300

    level manager

--- RTL/Bumpy/level_manager.sv
+++ RTL/Bumpy/level_manager.sv
@@ -9,6 +9,7 @@ module	level_manager	(
 					
 					output	logic died_screen,
 					output	logic win_screen,
+					output	logic reset_fsm_N,
 					output	logic [2:0] lvl,
 					output	logic next_lvl
 );
@@ -16,66 +17,86 @@ module	level_manager	(
 const int SCREEN_DURATION_SEC = 3;  
 
 
-logic died_flag,win_flag;
 logic [3:0] timer_died, timer_win;
-		
+
+logic flag_win,flag_died;
+
+assign reset_fsm_N = ~(win_screen || died_screen);
+
+// win screen		
 always_ff@(posedge clk or negedge resetN)
 begin
 		if(!resetN) begin
-			died_screen <= 0;
 			win_screen <= 0;
 			lvl <= 0;
 			next_lvl <= 0;
-			died_flag <= 0;
-			win_flag <= 0;
-			timer_died <= 0;
-			timer_win <= 0;
+			flag_win <= 0;
 		end
 		else begin
-			if(bumpy_died)
-				died_flag <= 1;
-			if(level_comp) begin
-				win_flag <= 1;
+			if(level_comp && (flag_win == 0)) begin
+				win_screen <= 1;
 				lvl <= lvl + 1;
+				next_lvl <= 1;
+				flag_win <= 1;
+			end
+			else if(timer_win >= SCREEN_DURATION_SEC - 1) begin
+				win_screen <= 0;
+				flag_win <= 0;
 			end
 		end
 end
 
-// died screen
-always_ff@(posedge one_sec)
+// died screen		
+always_ff@(posedge clk or negedge resetN)
 begin
-	if( timer_died >= SCREEN_DURATION_SEC - 1) begin
-		died_flag <= 0;
-		timer_died <= 0;
-		died_screen <= 0;
-	end
-	else if(died_flag) begin
-		timer_died <= timer_died + 1;
-		died_screen <= 1;
-	end
+		if(!resetN) begin
+			died_screen <= 0;
+			flag_died <= 0;
+		end
+		else begin
+			if(bumpy_died && (flag_died == 0)) begin
+				died_screen <= 1;
+				flag_died <= 1;
+			end
+			else if(timer_died >= SCREEN_DURATION_SEC - 1) begin
+				died_screen <= 0;
+				flag_died <= 0;
+			end
+		end
 end
 
-// win screen
-always_ff@(posedge one_sec)
+// win timer
+always_ff@(posedge one_sec or negedge resetN)
 begin
-	if( timer_win >= SCREEN_DURATION_SEC - 1) begin
-		win_flag <= 0;
+	if(!resetN) begin
 		timer_win <= 0;
-		win_screen <= 0;
-		next_lvl <= 0;
 	end
-	else if(win_flag) begin
-		timer_win <= timer_win + 1;
-		win_screen <= 1;
-		next_lvl <= 1;
+	else begin
+		if( timer_win >= SCREEN_DURATION_SEC - 1) begin
+			timer_win <= 0;
+		end
+		else if(win_screen) begin
+			timer_win <= timer_win + 1;
+		end
 	end
 end
 
 
-
+// died timer
+always_ff@(posedge one_sec or negedge resetN)
+begin
+	if(!resetN) begin
+		timer_died <= 0;
+	end
+	else begin
+		if( timer_died >= SCREEN_DURATION_SEC - 1) begin
+			timer_died <= 0;
+		end
+		else if(win_screen) begin
+			timer_died <= timer_died + 1;
+		end
+	end
+end
 endmodule 
-
-
-
 
 
--- RTL/Bumpy/step_controller.sv
+++ RTL/Bumpy/step_controller.sv
@@ -5,6 +5,8 @@ module	step_controller	(
 					input 	logic	[10:0] pixelX,// current VGA pixel 
 					input 	logic	[10:0] pixelY,
 					input 	logic gate,
+					input		logic [2:0] lvl,
+					input		logic next_lvl, 
 					
 					input		logic [10:0] bumpy_x,
 					input		logic [10:0] bumpy_y,
@@ -13,17 +15,8 @@ module	step_controller	(
 					output 	logic	[10:0] tileTopLeftY,
 					output 	logic [3:0] [2:0] area // area[0]=LEFT_TILE_TYPE | area[1]=UP_TILE_TYPE | area[2]=RIGHT_TILE_TYPE | area[3]=DOWN_TILE_TYPE
 );
-// Frame size
-const int x_FRAME_SIZE = 639 ;
-const int y_FRAME_SIZE = 479 ;
-// grid size
-const int x_GRID_SIZE = x_FRAME_SIZE;
-const int y_GRID_SIZE = y_FRAME_SIZE;
-//_______________________________
-//__________________________________
-parameter  int BRICK_WIDTH_X = 64;
-parameter  int BRICK_HEIGHT_Y = 64;
-//__________________________________
+
+
 parameter  int NUM_OF_ROWS = 7;
 parameter  int NUM_OF_COLS = 10;
 
@@ -33,23 +26,32 @@ const logic [2:0] FREE=3'b000, REGU=3'b001, GATE=3'b010, DEATH=3'b011, WALL=3'b1
 
 
 // Maps
-logic [0:NUM_OF_ROWS-1] [0:NUM_OF_COLS-1] [2:0] map0 = {
-	{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
-	{REGU,FREE,FREE,FREE,REGU,FREE,FREE,FREE,FREE,FREE},
-	{FREE,FREE,REGU,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
-	{FREE,REGU,FREE,REGU,FREE,REGU,FREE,REGU,FREE,REGU},
-	{FREE,FREE,FREE,FREE,FREE,FREE,REGU,FREE,FREE,FREE},
-	{REGU,FREE,REGU,REGU,FREE,FREE,FREE,FREE,REGU,FREE},
-	{FREE,REGU,REGU,REGU,FREE,REGU,FREE,REGU,FREE,REGU}
+logic [0:1] [0:NUM_OF_ROWS-1] [0:NUM_OF_COLS-1] [2:0] maps = {
+	{
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{REGU,FREE,FREE,FREE,REGU,FREE,FREE,FREE,FREE,FREE},
+		{FREE,FREE,REGU,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{FREE,REGU,FREE,REGU,FREE,REGU,FREE,REGU,FREE,REGU},
+		{FREE,FREE,FREE,FREE,FREE,FREE,REGU,FREE,FREE,FREE},
+		{REGU,FREE,REGU,REGU,FREE,FREE,FREE,FREE,REGU,FREE},
+		{FREE,REGU,REGU,REGU,FREE,REGU,FREE,REGU,FREE,REGU}
+	},
+	{
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE,FREE},
+		{REGU,REGU,REGU,REGU,REGU,REGU,REGU,REGU,REGU,REGU}
+	}
 };
+
 
 logic [0:NUM_OF_ROWS-1] [0:NUM_OF_COLS-1] [2:0] currentMap;
 
 int X_index_in_grid, y_index_in_grid;
 int X_bumpy_index_in_grid, y_bumpy_index_in_grid;
-
-logic	[10:0] topLeftY;
-logic inside_grid;
 
 assign X_index_in_grid = ((pixelX) >> 6);
 assign y_index_in_grid = ((pixelY) >> 6);
@@ -58,55 +60,29 @@ assign X_bumpy_index_in_grid = ((bumpy_x) >> 6);
 assign y_bumpy_index_in_grid = ((bumpy_y) >> 6);
 
 
-//======--------------------------------------------------------------------------------------------------------------=
+// tile clock
 always_ff@(posedge clk)
 begin
-		
-		case(X_bumpy_index_in_grid)
-			(NUM_OF_COLS-1): begin
-				area[2] <= WALL; // right
-				area[0] <= currentMap[y_bumpy_index_in_grid][X_bumpy_index_in_grid-1]; // left
-			end 
-			0: begin
-				area[2] <= currentMap[y_bumpy_index_in_grid][X_bumpy_index_in_grid+1]; // right
-				area[0] <= WALL; // left
-			end
-			default: begin
-				area[2] <= currentMap[y_bumpy_index_in_grid][X_bumpy_index_in_grid+1]; // right
-				area[0] <= currentMap[y_bumpy_index_in_grid][X_bumpy_index_in_grid-1]; // left
-			end
-		endcase
-		
-		case(y_bumpy_index_in_grid)
-			(NUM_OF_ROWS-1): begin
-				area[1] <= currentMap[y_bumpy_index_in_grid-1][X_bumpy_index_in_grid]; // up
-				area[3] <= DEATH; // down
-			end 
-			0: begin
-				area[1] <= WALL; // up
-				area[3] <= currentMap[y_bumpy_index_in_grid+1][X_bumpy_index_in_grid]; // down
-			end
-			default: begin
-				area[1] <= currentMap[y_bumpy_index_in_grid-1][X_bumpy_index_in_grid]; // up
-				area[3] <= currentMap[y_bumpy_index_in_grid+1][X_bumpy_index_in_grid]; // down
-			end
-		endcase
 		step_type <= (currentMap[y_index_in_grid][X_index_in_grid]);
 		tileTopLeftX	<= ((X_index_in_grid)<<6); //calculate relative offsets from top left corner of the brick
 		tileTopLeftY	<= ((y_index_in_grid)<<6); //calculate relative offsets from top left corner of the brick
 end 
 
-always_ff@(posedge gate or negedge resetN)
+// map change clock
+always_ff@(posedge clk or negedge resetN)
 begin
 		if(!resetN) begin
-			currentMap <= map0;
+			currentMap <= maps[0];
 		end
-		else if(gate) begin
-			currentMap[6][4] <= GATE; 
+		else begin 
+			if(gate) begin
+				currentMap[6][4] <= GATE; 
+			end
+			else if(next_lvl) begin
+				currentMap <= maps[lvl];
+			end
 		end
-end 
-
-
+end
 
 endmodule 
 
